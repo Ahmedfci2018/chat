87d57f7c5a5d5235afe7fa4562ddb3fde1333660
Ahmed Ali <ahmedfci2018@gmail.com>
2021-02-06 13:42:03 +0200

    upload project

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+
+
+    public function index($id = null)
+    {
+        // get current route
+        $route = (in_array(\Request::route()->getName(), ['user', config('chatify.path')]))
+            ? 'user'
+            : \Request::route()->getName();
+
+        // prepare id
+        return view('Chatify::pages.app', [
+            'id' => ($id == null) ? 0 : $route . '_' . $id,
+            'route' => $route,
+            'messengerColor' => Auth::user()->messenger_color,
+            'dark_mode' => Auth::user()->dark_mode < 1 ? 'light' : 'dark',
+        ]);
+    }
+}
